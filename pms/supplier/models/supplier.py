51d442d7f7797e4af889a89b01af5d1770bf52ed
wulei <crackwl@163.com>
2017-04-25 18:55:51 +0800

    fix add supplier bug

--- pms/supplier/models/supplier.py
+++ pms/supplier/models/supplier.py
@@ -15,7 +15,7 @@ def gen_vendor_code(start=None):
     # fuck, supplier can delete, so count+1 maybe the same with exist vendor code, 20170425
     supplier = SaleSupplier.objects.all().order_by('-created').first()
     if supplier and supplier.id > start:
-        start = supplier.id
+        start = supplier.id + 1
     return 'GYS%06d' % start
 
 class SaleSupplier(models.Model):
